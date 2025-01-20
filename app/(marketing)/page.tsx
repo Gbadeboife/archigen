11cab0b88c60b4622eabcc80b3b4f067c4ab966a
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-01-21 00:17:40 +0100

    update features section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,6 @@
 import { infos } from "@/config/landing";
 import BentoGrid from "@/components/sections/bentogrid";
+import FeaturesOld from "@/components/sections/tools";
 import Features from "@/components/sections/features";
 import HeroLanding from "@/components/sections/hero-landing";
 import InfoLanding from "@/components/sections/info-landing";
@@ -12,11 +13,12 @@ export default function IndexPage() {
     <>
       <HeroLanding />
       <PreviewLanding />
+      <Features />
       {/*<Powered />*/}
       <BentoGrid />
       <InfoLanding data={infos[0]} reverse={true} />
       {/* <InfoLanding data={infos[1]} /> */}
-      <Features />
+      <FeaturesOld />
       <Testimonials />
     </>
   );
