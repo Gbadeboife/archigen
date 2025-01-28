08a8957da3e5b9e54e82933d32572b1d7b13444c
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-01-28 21:58:48 +0100

    change to login modal

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -15,7 +15,7 @@ export default function RegisterPage() {
   return (
     <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
       <Link
-        href="/login"
+        href="/login" 
         className={cn(
           buttonVariants({ variant: "ghost" }),
           "absolute right-4 top-4 md:right-8 md:top-8"
@@ -38,7 +38,7 @@ export default function RegisterPage() {
           <Suspense>
             <UserAuthForm type="register" />
           </Suspense>
-          <p className="px-8 text-center text-sm text-muted-foreground">
+          {/*<p className="px-8 text-center text-sm text-muted-foreground">
             By clicking continue, you agree to our{" "}
             <Link
               href="/terms"
@@ -54,7 +54,7 @@ export default function RegisterPage() {
               Privacy Policy
             </Link>
             .
-          </p>
+          </p>*/}
         </div>
       </div>
     </div>
