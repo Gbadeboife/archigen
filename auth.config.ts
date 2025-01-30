2cbc3f8b879238b2b2d85f49e727d39199060dc1
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-01-30 23:31:11 +0100

    changes to auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,8 @@
 import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
-import Resend from "next-auth/providers/resend";
-
 import { env } from "@/env.mjs";
-import { sendVerificationRequest } from "@/lib/email";
+
+
 
 export default {
   providers: [
@@ -11,10 +10,11 @@ export default {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    Resend({
-      apiKey: env.RESEND_API_KEY,
+    /*Resend({
+      apiKey: env.RESEND_API_KEY,         
       from: env.EMAIL_FROM,
       // sendVerificationRequest,
-    }),
+    }),*/
   ],
+
 } satisfies NextAuthConfig;
