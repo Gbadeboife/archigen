6a52c7fdbcaf2b5d94b5c229e1290d97c2bb319e
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-02-01 11:36:18 +0100

    m

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,6 @@
 import { auth } from "@/auth"
-import { NextResponse } from "next/server"
-import type { NextRequestWithAuth } from "next-auth/middleware"
 
-export default auth((req: NextRequestWithAuth) => {
-  const isLoggedIn = !!req.auth
-  const isProtectedRoute = req.nextUrl.pathname.startsWith("/generate")
-
-  if (isProtectedRoute && !isLoggedIn) {
-    return NextResponse.redirect(new URL("/login", req.url))
-  }
-
-  return NextResponse.next()
-})
+export default auth
 
 export const config = {
   matcher: ["/generate"]
