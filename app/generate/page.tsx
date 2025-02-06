a9fe280acfbd89a28a3cc46edaca34f5e83775d4
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-02-06 11:05:47 +0100

    changes

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -4,12 +4,12 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function Generate() {
-    /*const session = await auth();
+    const session = await auth();
     
     if (!session?.user) {
         redirect("/login");
     }
-        */
+        
 
     return (
         <>
--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -57,7 +57,7 @@ export function UserAuthForm({ className, type, ...props }: UserAuthFormProps) {
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      {/*<form onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
@@ -86,16 +86,16 @@ export function UserAuthForm({ className, type, ...props }: UserAuthFormProps) {
             {type === "register" ? "Sign Up with Email" : "Sign In with Email"}
           </button>
         </div>
-      </form>
+      </form>*/}
       <div className="relative">
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
-        <div className="relative flex justify-center text-xs uppercase">
+        {/*<div className="relative flex justify-center text-xs uppercase">
           <span className="bg-background px-2 text-muted-foreground">
             Or continue with
           </span>
-        </div>
+        </div>*/}
       </div>
       <button
         type="button"
