a5531a68195d970f7492744a2e9c6f7bed73abad
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-02-12 21:46:14 +0100

    Update Renderer.tsx

--- components/mvp/Renderer.tsx
+++ components/mvp/Renderer.tsx
@@ -308,7 +308,7 @@ export default function Renderer() {
                 </div>
               </CardContent>
             </Card>
-            <div className="fixed bottom-4 left-0 w-full rounded-lg border bg-background px-4 lg:left-auto lg:right-[calc(66.666667%+1rem)] lg:w-[calc(33.333333%-2rem)] lg:px-0">
+            <div className="fixed bottom-4 left-0 w-full rounded-lg bg-background px-4 lg:left-auto lg:right-[calc(66.666667%+1rem)] lg:w-[calc(33.333333%-2rem)] lg:px-0">
               <Button className="w-full" size="lg" onClick={handleRender} disabled={isLoading || !uploadedImage}>
                 {isLoading ? <LoadingSpinner /> : "Render"}
               </Button>
