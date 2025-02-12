c598865d3a1c5172231cfe37dfaac90c257461fc
Gbadebo Ifedayo <gbadeboifedayo@gmail.com>
2025-02-12 21:09:16 +0100

    fix stupid button

--- components/mvp/Renderer.tsx
+++ components/mvp/Renderer.tsx
@@ -308,7 +308,7 @@ export default function Renderer() {
                 </div>
               </CardContent>
             </Card>
-            <div className="fixed bottom-4 w-full content-border rounded-lg border bg-background lg:left-auto lg:right-[calc(66.666667%+1rem)] lg:w-[calc(33.333333%-2rem)]">
+            <div className="fixed bottom-4 px-4 left-0 lg:px-0 w-full rounded-lg border bg-background lg:left-auto lg:right-[calc(66.666667%+1rem)] lg:w-[calc(33.333333%-2rem)]">
               <Button className="w-full" size="lg" onClick={handleRender} disabled={isLoading || !uploadedImage}>
                 {isLoading ? <LoadingSpinner /> : "Render"}
               </Button>
